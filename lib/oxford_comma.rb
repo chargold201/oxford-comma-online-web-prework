f1a95734c99faf935f2625341a15659666797a14
Charlotte Goldthwaite <chargold201@gmail.com>
2019-03-05 19:26:27 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,12 +5,6 @@ def oxford_comma(array)
     array.join(" and ")
   else
     string = ""
-    array.each_with_index do |item, i|
-      if i != -1
-        string << item + ", "
-      else
-        string << "and " + item
-      end
-    end
+    
   end
 end
